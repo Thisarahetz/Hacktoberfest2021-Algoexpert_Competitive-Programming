e6b40bae3cce381d455b469c8393fd86aaf5002e
[Thisara Hettikankanama] <[thisarayashodagcc@gmail.com]>
2021-10-11 16:14:35 +0530

    abstract_factory_pattern

---
+++ abstract_factory_pattern/ShapeFactory.java
@@ -0,0 +1,12 @@
+package abstract_factory_pattern;
+
+public class ShapeFactory {
+	
+	private static final String Shape = null;
+
+	public String getShape() {
+		return Shape;
+		
+	}
+
+}
---
+++ abstract_factory_pattern/Square.java
@@ -0,0 +1,12 @@
+package abstract_factory_pattern;
+
+public class Square implements Shape {
+
+	@Override
+	public void drow() {
+		// TODO Auto-generated method stub
+		System.out.println("Square class");
+		
+	}
+
+}
