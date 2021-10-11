a24d64a9ed665b0504ccd9981256e059fb449d4b
Asanka Gamage <asankagamage1998@gmail.com>
2021-10-11 19:03:17 +0530

    uppercase

---
+++ uppercase.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    char c = 'A';
+
+    printf("Uppercase English Letters:\n");
+    while(c <= 'Z')
+    {
+        printf("%c ", c);
+        c++;
+    }
+    printf("\n\n");
+    return 0;
+
+}
